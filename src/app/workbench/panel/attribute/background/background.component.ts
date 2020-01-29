886133a888c41c9337013d3c21c50a43e9f56dea
cabbagelol <nickmiku@foxmail.com>
2020-01-30 05:38:27 +0800

    更新

---
+++ src/app/workbench/panel/attribute/background/background.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'blive-attrbute-background',
+  templateUrl: './background.component.html',
+  styleUrls: ['./background.component.css']
+})
+
+export class BackgroundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
