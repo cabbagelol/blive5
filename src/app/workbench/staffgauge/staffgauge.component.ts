a0fef124ac0c1384718241d273f16065333c1e8e
Funny <32964708+Funny002@users.noreply.github.com>
2020-03-09 13:27:23 +0800

    修改 ui
    修改 ui

--- src/app/workbench/staffgauge/staffgauge.component.ts
+++ src/app/workbench/staffgauge/staffgauge.component.ts
@@ -5,148 +5,127 @@ import api from 'src/public/api';
 import {window} from "rxjs/operators";
 
 @Component({
-  selector: 'blive-staffgauge',
-  templateUrl: './staffgauge.component.html',
-  styleUrls: ['./staffgauge.component.css']
+    selector: 'blive-staffgauge',
+    templateUrl: './staffgauge.component.html',
+    styleUrls: ['./staffgauge.component.less']
 })
 export class StaffgaugeComponent implements OnInit {
-  @Input() data: any;
-  @Output('whiteboardsizeSize') whiteboardsizesize = new EventEmitter<any>();
+    @Input() data: any;
+    @Output('whiteboardsizeSize') whiteboardsizesize = new EventEmitter<any>();
 
-  whiteboardsizeList: any = api.blankSize;
+    whiteboardsizeList: any = api.blankSize;
 
-  radioValue = api.blankSize[2].name;
+    radioValue = api.blankSize[2].name;
 
-  radioCustomValue: any = [1000, 147];
+    radioCustomValue: any = [1000, 147];
 
-  ngOnInit() {
-    this.onReadyStaffgauge();
-  }
+    ngOnInit() {
+        this.onReadyStaffgauge();
+    }
 
-
-  Ruler: any  = { // 标尺
-    direction: false, // 方向
-    width: {x: 0, y: 0}, // x,y
-    Mouse: {x: 0, y: 0}, // x/y值
-    Top: {t: 0, w: 0, v: 0, v_: new Array(0)}, // 上标尺
-    Auto: {t: 0, w: 0, v: 0, v_: new Array(0)}, // 左/右标尺
-    Guides: {value: [], index: false}, // 参考线
-  };
-
-  length9_ = new Array(9);
-
-  Mouse = {x: 0, y: 0}; // 鼠标位置
-  $_Ruler_Guides_: any;
-
-  /**
-   * 初始标尺
-   */
-  onReadyStaffgauge () {
-    // 预定义 this_
-    const this_ = this;
-    // 初始化标尺
-    this.__initialization();
-    // 监听鼠标位置
-    document.onmousemove = e => {
-      this_.Mouse = {x: e.clientX, y: e.clientY}; // 赋值鼠标位
-      if (this_.Ruler.Mouse) {
-        const Mouse = {x: e.clientX - 25, y: e.clientY - 25};
-        Mouse.x += this_.Ruler.direction ? 25 : 0;
-        this_.Ruler.Mouse = Mouse;// 赋值鼠标位
-      }
+    Ruler: any = { // 标尺
+        width: {x: 0, y: 0}, // x,y
+        Mouse: {x: 0, y: 0}, // x/y值
+        Guides: {value: [], index: false}, // 参考线
+        Top: {t: 0, w: 0, v: 0, v_: new Array(0)}, // 上标尺
+        Auto: {t: 0, w: 0, v: 0, v_: new Array(0)}, // 左/右标尺
     };
-  }
 
-  __initialization() {
-    const self = this;
-    const window_ = $(window);
+    length9_ = new Array(9);
+    Mouse = {x: 0, y: 0}; // 鼠标位置
+    $_Ruler_Guides_: any;
 
-    // 获取
-    let [w, h] = [window_.width(), window_.height()];
-    // 修正
-    [w, h] = [w + 50 - w % 50, h + 50 - h % 50];
-    // 赋值
-    [
-      self.Ruler.Top.w, self.Ruler.Auto.w,
-      self.Ruler.Top.t, self.Ruler.Auto.t,
-      self.Ruler.Top.v, self.Ruler.Auto.v,
-      self.Ruler.Top.v_, self.Ruler.Auto.v_,
-    ] = [
-      w * 3, h * 3,
-      w - 201, h - 201,
-      w / 50 * 3, h / 50 * 3,
-      new Array(w / 50 * 3), new Array( h / 50 * 3),
-    ];
-  }
-
-  _Ruler_Mouse(is) { // 标尺鼠标位是否显示
-    this.Ruler.Mouse = is;
-  }
-
-  _Ruler_Guides_(is_level) { // 添加参考线
-    this.Ruler.Guides.value.push({ // 添加一个参考线
-      standard: is_level, // 是否水平（true/false）
-      distance: 0, // 距离
-    });
-
-    this._Ruler_Guides_dolly(this.Ruler.Guides.value.length - 1);
-  }
-
-  _Ruler_Guides_dolly(key) { // 参考线 ~ 移动
-    const this_ = this; // 预定义
-
-    if (key === false) {
-      const is_Guides = this_.Ruler.Guides.index;
-
-      if (is_Guides !== false) { // 参考线添加/移动共享方法
-        clearInterval(this_.$_Ruler_Guides_); // 取消执行
-
-        const Guides = this_.Ruler.Guides.value[is_Guides];
-        if (this_.Ruler.direction) {
-          if (Guides.standard && Guides.distance <= 20) {
-            this_.Ruler.Guides.value.splice(is_Guides, 1);
-          } else if (Guides.distance >= $('#App')[0].offsetWidthh - 25) {
-            this_.Ruler.Guides.value.splice(is_Guides, 1);
-          }
-        } else if (Guides.distance <= 20) { // 删除该元素
-          this_.Ruler.Guides.value.splice(is_Guides, 1);
-        }
-        this_.Ruler.Guides.index = false; // 取消定位
-      }
-    } else {
-      if (this_.$_Ruler_Guides_ >= 0) { // 有bug，如果存在修正
-        clearInterval(this_.$_Ruler_Guides_);
-      }
-
-      this_.Ruler.Guides.index = key; // 定位索引
-
-      this_.$_Ruler_Guides_ = setInterval(function () {
-        // 获取部分信息，赋值
-        const is = this_.Ruler.Guides.value[key].standard;
-        this_.Ruler.Guides.value[key].distance = is ? this_.Mouse.y : this_.Mouse.x;
-      }, 100); // 1ms执行速度
+    /**
+     * 初始标尺
+     */
+    onReadyStaffgauge() {
+        // 预定义 this_
+        const this_ = this;
+        // 初始化标尺
+        this.__initialization();
+        // 监听鼠标位置
+        document.onmousemove = e => {
+            this_.Mouse = {x: e.clientX, y: e.clientY}; // 赋值鼠标位
+            // 鼠标位
+            let is = true;
+            const Mouse = {x: e.clientX - 95, y: e.clientY - 25};
+            for (let k in Mouse) if (Mouse[k] < 0) is = false;
+            this_.Ruler.Mouse = is ? Mouse : false;
+        };
     }
-  }
 
-  /**
-   * 选择比例
-   * @param item
-   */
-  public setWhiteboardsizeSize (item: any) {
-    const self = this;
-    if (item == -1) {
-      self.whiteboardsizesize.emit({
-        name: '自定义',
-        width: self.radioCustomValue[0],
-        height: self.radioCustomValue[1],
-        company: 'px',
-      });
-    } else {
-      api.blankSize.forEach(i => {
-        if (i.name == self.radioValue) {
-          self.whiteboardsizesize.emit(i);
-        }
-      })
+    __initialization() {
+        const self = this;
+        const window_ = $(window);
+
+        // 获取
+        let [w, h] = [window_.width(), window_.height()];
+        // 修正
+        [w, h] = [w + 50 - w % 50, h + 50 - h % 50];
+        // 赋值
+        [
+            self.Ruler.Top.w, self.Ruler.Auto.w,
+            self.Ruler.Top.t, self.Ruler.Auto.t,
+            self.Ruler.Top.v, self.Ruler.Auto.v,
+            self.Ruler.Top.v_, self.Ruler.Auto.v_,
+        ] = [
+            w * 3, h * 3,
+            w - 201, h - 201,
+            w / 50 * 3, h / 50 * 3,
+            new Array(w / 50 * 3), new Array(h / 50 * 3),
+        ];
     }
-  }
+
+    _Ruler_Guides_(is_level) { // 添加参考线
+        this.Ruler.Guides.value.push({ // 添加一个参考线
+            standard: is_level, // 是否水平（true/false）
+            distance: 0, // 距离
+        });
+        this._Ruler_Guides_dolly(this.Ruler.Guides.value.length - 1);
+    }
+
+    _Ruler_Guides_dolly(key) { // 参考线 ~ 移动
+        console.log(this.Ruler.Guides.value[key]);
+        const this_ = this; // 预定义
+        if (key === false) {
+            const is_Guides = this_.Ruler.Guides.index;
+            if (is_Guides !== false) { // 参考线添加/移动共享方法
+                clearInterval(this_.$_Ruler_Guides_); // 取消执行
+                const Guides = this_.Ruler.Guides.value[is_Guides];
+                if (Guides.distance <= 95 && !Guides.standard || Guides.distance <= 25) {
+                    this_.Ruler.Guides.value.splice(is_Guides, 1);
+                }
+                this_.Ruler.Guides.index = false; // 取消定位
+            }
+        } else {
+            if (this_.$_Ruler_Guides_ >= 0) clearInterval(this_.$_Ruler_Guides_); // 有bug，如果存在修正
+            this_.Ruler.Guides.index = key; // 定位索引
+            this_.$_Ruler_Guides_ = setInterval(function () {
+                const is = this_.Ruler.Guides.value[key].standard;
+                this_.Ruler.Guides.value[key].distance = is ? this_.Mouse.y : this_.Mouse.x;
+            }, 100); // 1ms执行速度
+        }
+    }
+
+    /**
+     * 选择比例
+     * @param item
+     */
+    public setWhiteboardsizeSize(item: any) {
+        const self = this;
+        if (item == -1) {
+            self.whiteboardsizesize.emit({
+                name: '自定义',
+                width: self.radioCustomValue[0],
+                height: self.radioCustomValue[1],
+                company: 'px',
+            });
+        } else {
+            api.blankSize.forEach(i => {
+                if (i.name == self.radioValue) {
+                    self.whiteboardsizesize.emit(i);
+                }
+            })
+        }
+    }
 }
